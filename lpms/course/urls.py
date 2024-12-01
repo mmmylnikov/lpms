72837fd102e6f91455f20ebb27e31b6770e72e2c
Max Mylnikov <mmmylnikov@yandex.ru>
2024-12-01 13:42:31 +0500

    added custom error page templates

--- lpms/course/urls.py
+++ lpms/course/urls.py
@@ -1,7 +1,13 @@
 from django.urls import path
 
-from course.views import CourseDetailView
+from course.views import CourseDetailView, test_handler_error_view
+from config import settings
 
 urlpatterns = [
     path('', CourseDetailView.as_view(), name='home'),
+    ]
+
+if settings.DEBUG:
+    urlpatterns += [
+        path('test_handler_status/<int:status_code>', test_handler_error_view)
     ]
--- lpms/course/views.py
+++ lpms/course/views.py
@@ -1,5 +1,9 @@
+from typing import Any
+
 from django.contrib.auth.mixins import LoginRequiredMixin
 from django.views.generic.base import TemplateView
+from django.shortcuts import render
+from django.http import HttpRequest, HttpResponse
 from markdown import markdown
 
 from course.models import Course
@@ -43,3 +47,93 @@ class CourseDetailView(LoginRequiredMixin, TemplateView):
         if not course_id_raw:
             return Course.objects.first()
         return Course.objects.get(pk=int(course_id_raw))
+
+
+def handler_404_view(
+    request: HttpRequest, *args: Any, **kwargs: Any
+) -> HttpResponse:
+    status = 404
+    return render(
+        request,
+        "error.html",
+        {"status_code": status, "status_text": "Страница не найдена"},
+        status=status,
+    )
+
+
+def handler_500_view(
+    request: HttpRequest, *args: Any, **kwargs: Any
+) -> HttpResponse:
+    status = 500
+    return render(
+        request,
+        "error.html",
+        {"status_code": status, "status_text": "Сервис вернул ошибку"},
+        status=status,
+    )
+
+
+def handler_503_view(
+    request: HttpRequest, *args: Any, **kwargs: Any
+) -> HttpResponse:
+    status = 503
+    return render(
+        request,
+        "error.html",
+        {
+            "status_code": status,
+            "status_text": " ".join([
+                "Мы проводим техническое обслуживание LMS,",
+                "чтобы сделать сервис лучше. Скоро все заработает!"
+                ]),
+        },
+        status=status,
+    )
+
+
+def handler_403_view(
+    request: HttpRequest, *args: Any, **kwargs: Any
+) -> HttpResponse:
+    status = 403
+    return render(
+        request,
+        "error.html",
+        {
+            "status_code": status,
+            "status_text": "Доступ на эту страницу запрещен",
+        },
+        status=status,
+    )
+
+
+def handler_400_view(
+    request: HttpRequest, *args: Any, **kwargs: Any
+) -> HttpResponse:
+    status = 400
+    return render(
+        request,
+        "error.html",
+        {"status_code": status, "status_text": "Неправильный запрос"},
+        status=status,
+    )
+
+
+def test_handler_error_view(
+    request: HttpRequest, status_code: int
+) -> HttpResponse:
+    if status_code == 404:
+        return handler_404_view(request)
+    elif status_code == 500:
+        return handler_500_view(request)
+    elif status_code == 503:
+        return handler_503_view(request)
+    elif status_code == 403:
+        return handler_403_view(request)
+    elif status_code == 400:
+        return handler_400_view(request)
+    return render(
+        request,
+        "error.html",
+        {"status_code": status_code, "status_text": "Сервис вернул ошибку"},
+        status=status_code,
+    )
